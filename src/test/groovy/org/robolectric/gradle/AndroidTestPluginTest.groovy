d2c5fe3f4e685f786fea6a1159096ec0f43bb2c4
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2014-03-28 12:02:05 +0000

    reenabled ignored test, fixed whitespace to match project

--- src/test/groovy/org/robolectric/gradle/AndroidTestPluginTest.groovy
+++ src/test/groovy/org/robolectric/gradle/AndroidTestPluginTest.groovy
@@ -133,7 +133,6 @@ class AndroidTestPluginTest {
         return project
     }
 
-  @Ignore
   @Test public void parseInstrumentTestCompile_androidGradle_0_9_0() {
     String androidGradleTool = "com.android.tools.build:gradle:0.9.0"
     String configurationName = "androidTestCompile"
